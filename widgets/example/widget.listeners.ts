e9de5b0f1804678fb32feced70cde3b655c6cbc3
Timo Westkämper <timo.westkamper@nosto.com>
2024-07-23 11:11:59 +0300

    Minor fixes

--- widgets/example/widget.listeners.ts
+++ widgets/example/widget.listeners.ts
@@ -4,9 +4,9 @@ import { getLoadLessButton, getLoadMoreButton } from "./widget.buttons"
 declare const sdk: Sdk
 
 export function loadListeners() {
-  const buttons = sdk.querySelector("#buttons")
-  const postLoad = sdk.querySelector("#postload")
-  const preLoad = sdk.querySelector("#preload")
+  const buttons = sdk.querySelector("#buttons")!
+  const postLoad = sdk.querySelector("#postload")!
+  const preLoad = sdk.querySelector("#preload")!
 
   sdk.addEventListener("tilesUpdated", () => {
     if (sdk.tiles.hasMorePages()) {
