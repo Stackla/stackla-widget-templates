fcd809988fb42f5b8a48abd2bb5aa7349457733a
aistetamos <aiste@searchnode.com>
2024-11-27 13:21:50 +0200

    updates

--- widgets/waterfall/waterfall.lib.ts
+++ widgets/waterfall/waterfall.lib.ts
@@ -1,10 +1,8 @@
 import { ISdk } from "@stackla/widget-utils"
+import { ShareMenu } from "packages/widget-utils/src/libs/templates/share-menu/share-menu.lib"
+import { copyToClipboard } from "packages/widget-utils/src/libs/templates/share-menu/share-menu.listener"
 
 declare const sdk: ISdk
-
-export function reinitialiseWaterfallLayout() {
-  loadWaterfallLayout(true)
-}
 
 export function loadWaterfallLayout(reset = false) {
   const allTiles = Array.from(sdk.querySelectorAll<HTMLElement>(".grid-item") ?? [])
@@ -30,5 +28,56 @@ export function loadWaterfallLayout(reset = false) {
       tile.setAttribute("height-set", "true")
       tile.classList.add("processed")
     }
+
+    const shareButton = tile.querySelector(".icon-share")
+    shareButton?.addEventListener("click", () => {
+      const tileId = tile.getAttribute("data-id")
+      if (tileId) {
+        const matchingTile = sdk.tiles.tiles[tileId]
+        const container = sdk.querySelector("#nosto-ugc-container")
+
+        const existingShareMenu = sdk.querySelector(".share-socials-popup-wrapper")
+        if (existingShareMenu) {
+          existingShareMenu.remove()
+        }
+
+        container?.appendChild(ShareMenu({ tile: matchingTile }))
+
+        let overlayElement = container.querySelector(".overlay") as HTMLElement
+
+        if (!overlayElement) {
+          overlayElement = document.createElement("div")
+          overlayElement.classList.add("overlay")
+          container.appendChild(overlayElement)
+        }
+
+        overlayElement.addEventListener("click", () => {
+          shareMenuWrapper.style.display = "none"
+
+          if (container.contains(overlayElement)) {
+            container.removeChild(overlayElement)
+          }
+        })
+
+        const shareMenuWrapper = sdk.querySelector(".share-socials-popup-wrapper")
+        shareMenuWrapper.style.display = "flex"
+
+        const clipboardElement = shareMenuWrapper?.querySelector<HTMLElement>(".url-copy .copy-button")
+        const shareUrlElement = shareMenuWrapper?.querySelector<HTMLInputElement>(".url-copy .share-url")
+
+        clipboardElement?.addEventListener("click", async () => {
+          if (shareUrlElement) {
+            await copyToClipboard(shareUrlElement)
+          }
+        })
+
+        const closeButton = shareMenuWrapper.querySelector<HTMLElement>(".share-modal-exit")
+        closeButton?.addEventListener("click", closeEvent => {
+          closeEvent.preventDefault()
+          shareMenuWrapper.style.display = "none"
+          container.removeChild(overlayElement)
+        })
+      }
+    })
   })
 }
--- widgets/waterfall/widget.tsx
+++ widgets/waterfall/widget.tsx
@@ -1,19 +1,16 @@
-import { loadAllUnloadedTiles } from "@stackla/widget-utils/extensions/swiper"
-import { ISdk, loadWidget } from "@stackla/widget-utils"
+import { loadWidget } from "@stackla/widget-utils"
 import shopspotStyle from "./components/shopspot-icon/base.scss"
-import { reinitialiseWaterfallLayout, loadWaterfallLayout } from "./waterfall.lib"
-import { ShareMenu } from "packages/widget-utils/src/libs/templates/share-menu/share-menu.lib"
-import { copyToClipboard } from "packages/widget-utils/src/libs/templates/share-menu/share-menu.listener"
-
-declare const sdk: ISdk
+import { loadWaterfallLayout } from "./waterfall.lib"
 
 loadWidget({
   extensions: {},
   features: {},
   callbacks: {
-    onLoadMore: [() => loadWaterfallLayout()],
-    onTilesUpdated: [() => loadWaterfallLayout()],
-    onResize: [() => reinitialiseWaterfallLayout()]
+    onTilesUpdated: [
+      () => {
+        loadWaterfallLayout()
+      }
+    ]
   },
   templates: {
     "expanded-tiles": {
@@ -27,55 +24,4 @@ loadWidget({
   }
 })
 loadWaterfallLayout()
-loadAllUnloadedTiles()
-
-
-sdk.querySelectorAll(".ugc-tile").forEach(tile => {
-  const shareButton = tile.querySelector(".icon-share")
-  shareButton?.addEventListener("click", () => {
-    const tileId = tile.getAttribute("data-id")
-    if (tileId) {
-      const matchingTile = sdk.tiles.tiles[tileId]
-      const container = sdk.querySelector("#nosto-ugc-container")
-
-      const existingShareMenu = sdk.querySelector(".share-socials-popup-wrapper")
-      if (existingShareMenu) {
-        existingShareMenu.remove()
-      }
-
-
-      container?.appendChild(ShareMenu({ tile: matchingTile }))
-
-      let overlayElement = container.querySelector(".overlay") as HTMLElement
-      if (!overlayElement) {
-        overlayElement = document.createElement("div")
-        overlayElement.classList.add("overlay")
-        container.appendChild(overlayElement)
-      }
-
-      overlayElement.addEventListener("click", () => {
-        shareMenuWrapper.style.display = "none"
-        container.removeChild(overlayElement)
-      })
-
-      const shareMenuWrapper = sdk.querySelector(".share-socials-popup-wrapper")
-      shareMenuWrapper.style.display = "flex"
-
-      const clipboardElement = shareMenuWrapper?.querySelector<HTMLElement>(".url-copy .copy-button")
-      const shareUrlElement = shareMenuWrapper?.querySelector<HTMLInputElement>(".url-copy .share-url")
-
-      clipboardElement?.addEventListener("click", () => {
-        if (shareUrlElement) {
-          copyToClipboard(shareUrlElement)
-        }
-      })
-
-      const closeButton = shareMenuWrapper.querySelector<HTMLElement>(".share-modal-exit")
-      closeButton?.addEventListener("click", closeEvent => {
-        closeEvent.preventDefault()
-        shareMenuWrapper.style.display = "none"
-        container.removeChild(overlayElement)
-      })
-    }
-  })
-})
+// loadAllUnloadedTiles()
