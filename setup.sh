9ab4c379f6f4e2998b304d7e3347ffd7172969e9
Oliver Kucharzewski <oliver@olidev.com.au>
2025-04-30 11:24:40 +1000

    chore: enhance setup script to add and merge Stackla widget templates

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ git submodule init
 git submodule update --remote --recursive
 echo "Installing dependencies..."
 npm install
+echo "Adding Stackla widget templates as a remote..."
+git remote add stackla-origin https://github.com/Stackla/stackla-widget-templates
+git fetch stackla-origin
+echo "Merging Stackla widget templates..."
+git merge stackla-origin/master
