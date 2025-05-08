7359c0870023a22b35d91b202f911dc4bcc9acfb
Oliver Kucharzewski <oliver@olidev.com.au>
2025-05-08 17:25:04 +1000

    Refactor inline story swiper initialization and update slides per view logic

--- widgets/storyline/slides-per-view.ts
+++ widgets/storyline/slides-per-view.ts
@@ -3,16 +3,15 @@ import { getTileWidthBySizeString } from "@stackla/widget-utils"
 
 declare const sdk: Sdk
 
-export function getSlidesPerView(screenWidth: number = window.innerWidth): number {
+export function getSlidesPerView(screenWidth: number = sdk.querySelector("#nosto-ugc-container").clientWidth): number {
   const {
     enable_custom_tiles_per_page: isCustomTilesPerPageEnabled,
     tiles_per_page: tilesPerPage,
     inline_tile_size: tileSizeString
   } = sdk.getStyleConfig()
 
-  screenWidth = Math.min(screenWidth, 1130)
   const tileSize = getTileWidthBySizeString(tileSizeString).replace("px", "")
-  const tileSizeWithMargin = Number(tileSize)
+  const tileSizeWithMargin = Number(tileSize) - 30
   const slidesPerView = Math.ceil(screenWidth / tileSizeWithMargin)
 
   if (isCustomTilesPerPageEnabled && tilesPerPage) {
