253d2a53d96f1def3df8b6ee59ef85ac6e602105
Oliver Kucharzewski <oliver@olidev.com.au>
2025-07-29 15:52:03 +1000

    fix: setup script to stash changes before merging Stackla widget templates

--- setup.sh
+++ setup.sh
@@ -8,4 +8,7 @@ echo "Adding Stackla widget templates as a remote..."
 git remote add stackla-origin https://github.com/Stackla/stackla-widget-templates
 git fetch stackla-origin
 echo "Merging Stackla widget templates..."
+git stash
 git merge stackla-origin/master
+git stash pop
+echo "Setup complete. You can now start using the Stackla widget templates."
