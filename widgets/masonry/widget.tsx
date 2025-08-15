55626926b36537d250758257bbcff76adf4f6970
Oliver Kucharzewski <oliver@olidev.com.au>
2025-08-15 10:42:09 +1000

    feat: implement masonry layout functionality with tile rendering and error handling

--- widgets/masonry/widget.tsx
+++ widgets/masonry/widget.tsx
@@ -1,9 +1,25 @@
 import { ISdk, loadWidget } from "@stackla/widget-utils"
+import { handleAllTileImageRendered, handleTileImageError, renderMasonryLayout } from "./masonry.extension"
 
 declare const sdk: ISdk
 
 loadWidget(sdk, {
-  extensions: {
-    masonry: true
+  callbacks: {
+    onTilesUpdated: [() => renderMasonryLayout(sdk)],
+    onTileBgImgRenderComplete: [
+      () => {
+        handleAllTileImageRendered(sdk)
+        setTimeout(() => handleAllTileImageRendered(sdk), 1000)
+      }
+    ],
+    onTileBgImageError: [
+      event => {
+        const customEvent = event
+        const tileWithError = customEvent.detail.data.target as HTMLElement
+        handleTileImageError(sdk, tileWithError)
+      }
+    ]
   }
 })
+
+renderMasonryLayout(sdk)
