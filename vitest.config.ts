9143b5ef338b4f0c6a144390a4145452e198ea55
Oliver Kucharzewski <oliver@olidev.com.au>
2025-09-30 14:36:27 +1000

    fix: update test command to run with no tests and adjust test file inclusion pattern

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
-    include: ["src/**/*.spec.ts", "src/**/*.test.ts"]
+    include: ["widgets/**/*.spec.ts", "widgets/**/*.test.ts"]
   }
 })
