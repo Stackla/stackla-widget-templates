f38c3ba13ff8427e2d2ae24acec3a3e13a37520d
Oliver Kucharzewski <oliver@olidev.com.au>
2025-10-02 13:51:37 +1000

    test: enhance product navigation checks in shouldNavigateProducts function

--- tests/e2e/shared-tests/products.ts
+++ tests/e2e/shared-tests/products.ts
@@ -22,4 +22,19 @@ export async function shouldNavigateProducts(page: Page, widgetType: string): Pr
   await leftArrow.click()
   await page.getByLabel("View product: Desna Dress").first().waitFor({ state: "visible" })
   await page.getByLabel("View product: Kathmandu 1").first().waitFor({ state: "hidden" })
+
+  // go back to the start
+  await rightArrow.click()
+  await page.getByLabel("View product: Kathmandu 1").first().waitFor({ state: "visible" })
+  await page.getByLabel("View product: Desna Dress").first().waitFor({ state: "hidden" })
+
+  // Click the third image to check that the image is visible when clicked
+  await page.getByLabel("Product image container: 43").getByRole("img", { name: "Product image" }).click()
+  await page.getByLabel("View product: Kathmandu 3").first().waitFor({ state: "visible" })
+  await page.getByLabel("Product details: Kathmandu 3").getByText("another pair").waitFor({ state: "visible" })
+
+  // Click the first image to check that the image is visible when clicked
+  await page.getByLabel("Product image container: 41").getByRole("img", { name: "Product image" }).click()
+  await page.getByLabel("View product: Kathmandu 1").first().waitFor({ state: "visible" })
+  await page.getByText("You should look for shoes").waitFor({ state: "visible" })
 }
