af700f8c8501aaa27d33a03f78de09e25feab5b3
Oliver Kucharzewski <oliver@olidev.com.au>
2025-10-13 10:48:06 +1100

    fix: Remove trailing commas from IDraftRequest interface properties

--- src/libs/interfaces.ts
+++ src/libs/interfaces.ts
@@ -11,8 +11,8 @@ export interface IDraftRequest {
   }
   customCSS: string
   customJS: string
-  widgetOptions: typeof widgetOptions,
-  widgetType: string;
+  widgetOptions: typeof widgetOptions
+  widgetType: string
 }
 
 export type PreviewContent = {
